9612c41103dec941ef3e7b6aa4ce1f3c5e03268f
ak-anonymous <83087214+ak-anonymous@users.noreply.github.com>
2021-05-05 23:32:56 +0530

    Add files via upload
    iniial

---
+++ fun.js
@@ -0,0 +1,10 @@
+function submit() {
+ document.getElementById("form11").innerHTML="Thank You For submitting your request"
+    // document.getElementById("form11").style.background="green"
+    // document.getElementById("form11").style.color="white"
+    document.getElementById("form11").classList.add("bg-red")
+    
+
+
+};
+document.getElementsByTagName
